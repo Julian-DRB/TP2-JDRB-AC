2bed0c713c25bea5be58adc563d7e893d9ea8f92
Dru-og <2511900@cegeplimoilou.ca>
2026-03-29 20:36:15 -0400

    Création de la classe OuvrageAudio.
    Création de la sous-classe OuvrageAudio, avec son constucteur, ses getters et setters, un format toString, ainsi qu'une validation d'une duréeMinute positive.

--- src/livres/OuvrageAudio.java
+++ src/livres/OuvrageAudio.java
@@ -1,5 +1,36 @@
 package livres;
 
 public class OuvrageAudio extends Ouvrage{
-    
+    private int dureeMinutes;
+    private String format;
+
+    public OuvrageAudio(String titre, Auteur auteur, int dureeMinutes, String format) {
+        super(titre, auteur);
+        setDureeMinutes(dureeMinutes);
+        setFormat(format);
+    }
+
+    public int getDureeMinutes() {
+        return dureeMinutes;
+    }
+
+    public void setDureeMinutes(int dureeMinutes) {
+        if (dureeMinutes < 0) {
+            System.out.println("La durée doit être positive")
+        }
+        this.dureeMinutes = dureeMinutes;
+    }
+
+    public String getFormat() {
+        return format;
+    }
+
+    public void setFormat(String format) {
+        this.format = format;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " - Audio : " + dureeMinutes + " min, Format : " + format;
+    }
 }

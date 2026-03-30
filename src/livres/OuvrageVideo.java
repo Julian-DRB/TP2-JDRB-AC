2e32cde3b96b828002b4c537fdedbfc219550c91
Julian-DRB <juliro2002@hotmail.com>
2026-03-29 22:56:00 -0400

    Ajout methodes et getter OuvrageVideo

--- src/livres/OuvrageVideo.java
+++ src/livres/OuvrageVideo.java
@@ -1,4 +1,34 @@
 package livres;
 
+import java.time.LocalDate;
+
 public class OuvrageVideo extends Ouvrage {
+
+    private int dureeMinutes;
+    private int tailleMb;
+
+    public OuvrageVideo(String titre, Auteur auteur, LocalDate date, int nombreExemplaires, int dureeMinutes, int tailleMb) {
+        super(titre, auteur, date, nombreExemplaires);
+        this.dureeMinutes = dureeMinutes;
+        this.tailleMb = tailleMb;
+    }
+
+    public OuvrageVideo(String titre, Auteur auteur, int dureeMinutes, int tailleMb) {
+        super(titre, auteur);
+        this.dureeMinutes = dureeMinutes;
+        this.tailleMb = tailleMb;
+    }
+
+    public int getDureeMinutes() {
+        return dureeMinutes;
+    }
+
+    public int getTailleMb() {
+        return tailleMb;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " - " + dureeMinutes + " min - " + tailleMb + " Mb";
+    }
 }

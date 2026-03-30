6ae05f3f1e35c5befe867c651c9b0dc69de790b5
Julian-DRB <juliro2002@hotmail.com>
2026-03-29 22:53:58 -0400

    Ajout classe OuvragePapier

--- src/livres/OuvragePapier.java
+++ src/livres/OuvragePapier.java
@@ -1,6 +1,27 @@
 package livres;
 
+import java.time.LocalDate;
+
 public class OuvragePapier extends Ouvrage {
 
+    private int nombrePages;
 
+    public OuvragePapier(String titre, Auteur auteur, LocalDate date, int nombreExemplaires, int nombrePages) {
+        super(titre, auteur, date, nombreExemplaires);
+        this.nombrePages = nombrePages;
+    }
+
+    public OuvragePapier(String titre, Auteur auteur, int nombrePages) {
+        super(titre, auteur);
+        this.nombrePages = nombrePages;
+    }
+
+    public int getNombrePages() {
+        return nombrePages;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " - " + nombrePages + " pages";
+    }
 }
